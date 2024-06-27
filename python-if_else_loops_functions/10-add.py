736b7957d2bb100f01f0907743dce8318f030618
S1rDavid9 <d.akachi@alustudent.com>
2024-06-27 03:09:40 -0700

    a+b

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def add(a, b):
-    return a + b
-
-# Example usage:
-result = add(3, 5)  # Output: 8
-print(result)
-
-result = add(-2, 7)  # Output: 5
-print(result)
+    sum = a + b
+    return(sum)
