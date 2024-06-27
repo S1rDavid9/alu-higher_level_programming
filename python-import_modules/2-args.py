e41cd64000f8a5115627e7e40874fefc3448e32c
S1rDavid9 <d.akachi@alustudent.com>
2024-06-27 07:10:27 -0700

    arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import sys
+if __name__ == "__main__":
+
+    argv = sys.argv[1:]  # Get the arguments excluding the script name
+    argc = len(argv)
+
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc))
+
+    for i, arg in enumerate(argv, start=1):
+        print("{}: {}".format(i, arg))
+
+   
+
