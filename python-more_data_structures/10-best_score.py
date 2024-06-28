adcdb246a43358b133d1cb03a50f5609a2820b48
S1rDavid9 <d.akachi@alustudent.com>
2024-06-28 04:21:38 -0700

    best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
         return None
 
     best_key = None
-    max_score = float('-inf')  # Start with negative infinity to handle all negative scores
+    max_score = float('-inf')  
 
     for key, value in a_dictionary.items():
         if isinstance(value, int) and value > max_score:
