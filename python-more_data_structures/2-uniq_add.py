12fac2a60e84cf1d22d2e18e7ccee2da299d85d5
S1rDavid9 <d.akachi@alustudent.com>
2024-06-28 03:06:50 -0700

    unique addition

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    # Create a set to store unique elements
+    unique_elements = set(my_list)
+    # Sum the elements in the set
+    total = sum(unique_elements)
+    return total
