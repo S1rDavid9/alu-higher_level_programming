8d0d453cbf725f4f5a7c7e0c90a0dde632a53f5e
S1rDavid9 <d.akachi@alustudent.com>
2024-07-06 08:38:12 -0700

    simple rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Rectangle:
+        """
+            A class used to represent a Rectangle.
+
+                Attributes
+                    ----------
+                        None
+                       """     
+pass
