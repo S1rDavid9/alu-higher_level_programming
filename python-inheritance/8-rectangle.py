c85ac5d8ae634bfdd3625f8704e99ef0da19384d
S1rDavid9 <d.akachi@alustudent.com>
2024-07-13 08:29:32 -0700

    rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,49 +1,18 @@
 #!/usr/bin/python3
-"""
-This module defines a class `BaseGeometry` and a subclass `Rectangle`.
-"""
+""" Base Geometry Class inherited"""
 
 
-class BaseGeometry:
-    """A class for geometry operations."""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
-    def area(self):
-        """Raises an exception indicating that the area method \
-                is not implemented."""
-        raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
-        """
-        Validates that value is a positive integer.
+""" Program that creates a Rectangle and intance its values """
 
-        Args:
-            name (str): The name of the value.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less or equal to 0.
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
 
 class Rectangle(BaseGeometry):
-    """A class representing a rectangle."""
-
+    """ class Rectangle that inherits from BaseGeometry """
     def __init__(self, width, height):
-        self.__width = 0
-        self.__height = 0
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """ Constructor """
         self.__width = width
         self.__height = height
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
-
-if __name__ == "__main__":
-    r = Rectangle(3, 5)
-    print(r)
-    print(f"Area: {r.area()}")
+        BaseGeometry.integer_validator(self, "width", self.__width)
+        BaseGeometry.integer_validator(self, "height", self.__height)
