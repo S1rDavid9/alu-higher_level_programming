a6c8a765405537fe6b2a4102cd2bc9d4655150a8
S1rDavid9 <d.akachi@alustudent.com>
2024-07-20 12:07:27 -0700

    first table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- This creates a table called first_table in the current database in your MySQL server
+-- Not allowed to use the SELECT or SHOW statements
+
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
