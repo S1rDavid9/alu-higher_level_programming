9d650c1ea202ce98bfa9d1737007436299d1f4ad
S1rDavid9 <d.akachi@alustudent.com>
2024-07-20 12:45:01 -0700

    list all in table

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,3 +1,3 @@
 -- This script lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server
 
-mysql -u your_username -p -D hbtn_0c_0 -e "SELECT * FROM first_table;"
+SELECT * FROM first_table;
