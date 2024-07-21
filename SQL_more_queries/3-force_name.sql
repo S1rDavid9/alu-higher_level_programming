37a5b4c5a4d0d6629cd4263e29c1826c12d11a5b
S1rDavid9 <d.akachi@alustudent.com>
2024-07-21 10:28:05 -0700

    always a name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,9 +1,7 @@
--- Use the specified database
-USE ${DB_NAME};
+-- A script that create the table force_name on your MySQL server
 
 -- Create the table if it doesn't already exist
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
 );
-
