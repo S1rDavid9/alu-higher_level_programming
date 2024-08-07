86fe268b4c4b4118dd7ca4755f992593cbca43f2
S1rDavid9 <d.akachi@alustudent.com>
2024-08-07 08:18:29 -0700

    first state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-'''class definition of a state'''
+
+"""
+This module defines a State class and creates a MySQL table `states`
+using SQLAlchemy ORM. The table includes columns for id and name.
+
+Usage:
+    - Ensure SQLAlchemy is installed: pip install SQLAlchemy
+    - Replace <username>, <password>, and <database_name> with your MySQL credentials
+    - Run the script to create the table in the specified MySQL database
+"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 Base = declarative_base()
@@ -7,7 +16,7 @@ Base = declarative_base()
 
 class State(Base):
         '''class defination of a table'''
-            __tablename__ = 'states'
+         __tablename__ = 'states'
 
-                id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-                    name = Column(String(128), nullable=False)
+         id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+         name = Column(String(128), nullable=False)
