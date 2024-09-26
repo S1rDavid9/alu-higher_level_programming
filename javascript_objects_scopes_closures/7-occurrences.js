dc4a36167067779853c69e87c4d8ed569cb708da
S1rDavid9 <d.akachi@alustudent.com>
2024-09-26 00:37:26 -0700

    Occurences

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -3,9 +3,9 @@
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;  // Initialize the count to 0
 
-  // Loop through the list
+// Loop through the list
   for (let i = 0; i < list.length; i++) {
-    // If the current element matches the searchElement, increment the count
+// If the current element matches the searchElement, increment the count
     if (list[i] === searchElement) {
       count++;
     }
