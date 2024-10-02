65580838fe179e5839ebbee1b8660af5057f2069
S1rDavid9 <d.akachi@alustudent.com>
2024-10-02 05:17:01 -0700

    readme

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,18 +1,12 @@
 #!/usr/bin/node
+#!/usr/bin/node
 
-// Import the required module
 const fs = require('fs');
 
-// Retrieve the file path from command line arguments
-const filePath = process.argv[2];
-
-// Read the file content in utf-8 format
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', function (err, result) {
   if (err) {
-    // If an error occurs, print the error object
     console.log(err);
   } else {
-    // Print the content of the file
-    console.log(data);
+    console.log(result);
   }
 });
