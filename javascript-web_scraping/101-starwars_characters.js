8b32671ae38187c60eae313a888dcc19814b5c4b
S1rDavid9 <d.akachi@alustudent.com>
2024-10-05 08:11:28 -0700

    starwars right order

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -11,16 +11,27 @@ request(apiUrl, (error, response, body) => {
     const movieData = JSON.parse(body);
     const characters = movieData.characters;
 
-    // Loop through the list of character URLs in order
-    characters.forEach((characterUrl) => {
-      request(characterUrl, (err, res, body) => {
-        if (err) {
-          console.error(err);
-        } else {
-          const characterData = JSON.parse(body);
-          console.log(characterData.name);
-        }
+    // Array to hold all promises for fetching character names
+    const characterPromises = characters.map(characterUrl => {
+      return new Promise((resolve, reject) => {
+        request(characterUrl, (err, res, body) => {
+          if (err) {
+            reject(err);
+          } else {
+            const characterData = JSON.parse(body);
+            resolve(characterData.name);
+          }
+        });
       });
     });
+
+    // Wait for all promises to complete and then print the character names
+    Promise.all(characterPromises)
+      .then(characterNames => {
+        characterNames.forEach(name => console.log(name));
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 });
