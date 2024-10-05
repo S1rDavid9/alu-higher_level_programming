f0f104d19efd38fc9e66154980afef48b4f0c57a
S1rDavid9 <d.akachi@alustudent.com>
2024-10-05 08:02:23 -0700

    starwars right order

---
+++ javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const movieData = JSON.parse(body);
+    const characters = movieData.characters;
+
+    // Loop through the list of character URLs in order
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (err, res, body) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const characterData = JSON.parse(body);
+          console.log(characterData.name);
+        }
+      });
+    });
+  }
+});
